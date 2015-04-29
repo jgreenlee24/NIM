b198dd93957efff0e8145ba05e3d85d38ba19782
Benthescott <bcline@harding.edu>
2015-04-29 03:22:20 -0500

    NIMserver.h
    updated nim.h

--- Server.h
+++ Server.h
@@ -1,23 +1,34 @@
-#include <WinSock2.h>
-#include <string>
+#include <winsock2.h>
 
-static char Nim_UDPPORT[] = "29333";
+static char BROADCAST_ADDR[] = "10.1.255.255";
+static char UDPPORT_NIM[] = "29333";
+static char TCPPORT_NIM[] = "29334";
 
-const int MAX_SEND_BUF  = 2048;
-const int MAX_RECV_BUF  = 2048;
-const int MAX_SERVERS   = 100;
-
-const int MAXHOSTNAME = 80;
-const int TIMEBUF_SIZE = 26;
+const int MAX_SEND_BUF     = 2048;
+const int MAX_RECV_BUF     = 2048;
+const int MAX_HOSTNAME     = 1024;
+const int MAX_HOST_SIZE    = 16;
+const int MAX_SERVICE_SIZE = 80;
+const int MAX_LINE      = 1024;
+const int MAX_NAME      = 1024;
+const int MAX_HOST      = 100;
 
 #define MAXBUF  4096
 #define MAXIPBUF  20
 #define Nim_QUERY "Name?\0"
-#define Nim_CHALLENGE "Play? Name=client_name\0"
-#define Nim_ACK "Great!\0"
+#define Nim_CHALLENGE "Play?\0"
+#define Nim_ACK "GREAT!\0"
 
-SOCKET passivesock (char*,char*);
-int UDP_recv (SOCKET, char*, int, char*, char*);
-int UDP_send (SOCKET, char*, int, char*, char*);
-int wait(SOCKET, int, int);
-char* GetHostIP();
+void   chomp (char*);
+char*  timestamp(void);
+void   server_main(int,char*[]);
+void   client_main(int,char*[]);
+void   play_Nim(SOCKET, bool, char*);
+int    UDP_recv(SOCKET, char*, int, char*, char*);
+int    UDP_send (SOCKET, char*, int, char*, char*);
+int    wait(SOCKET, int, int);
+SOCKET passivesock (char*, char*);
+SOCKET connectsock (char*, char*, char*);
+
+
+
